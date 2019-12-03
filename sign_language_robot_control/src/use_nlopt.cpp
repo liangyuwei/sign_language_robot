3a65b76fd6c05e2d1834c0fe06acf98bd933d156
liangyuwei <liangyuwei@yahoo.com>
2019-12-03 16:56:37 +0800

    Comple the structure of NLopt for IK, the automatic differentiation of Loss function and Constraint functions are yet to be implemented.

--- sign_language_robot_control/src/use_nlopt.cpp
+++ sign_language_robot_control/src/use_nlopt.cpp
@@ -1,27 +1,45 @@
 #include <math.h>
 #include <nlopt.h>
 #include <vector>
+#include <iostream>
 
 int count = 0;
 
 typedef struct {
-  double vel_limits[], acc_limits[];
-  double jlb[], jub[];
+
+  std::vector<double> qlb, qub;
+  std::vector<double> q_previous;
+
 } my_constraint_data;
+
 
 
 // Loss function
 double myfunc(const std::vector<double> &x, std::vector<double> &grad, void *f_data)
 {
 
-  printf("Evaluation %d\n", count);
-  // Counter
+  // Counter information
   ++count;
+  std::cout << "Evaluation " << count << std::endl;
 
+
+  // Get additional information by typecasting void* f_data(user-defined data)
+  //
+
+
+  // Compute the configurations of elbow and wrist links
+  
+
+
+  // Calculate loss function
+
+
+
+  // Compute gradient using CppAD
   // only compute gradient if not NULL(???)
-  if(grad)
+  if(!grad.empty())
   {
-
+    
   }
 
   return 
@@ -29,18 +47,35 @@ double myfunc(const std::vector<double> &x, std::vector<double> &grad, void *f_d
 
 }
 
-// Constraint function; expected to be myconstraint(x)<=0
-double myconstraint(const double *x, double *grad, void *data)
-{
 
+// Constraint function; expected to be myconstraint(x)<=0
+void myconstraint(unsigned m, double *result, unsigned n, const double *x,
+                             double *grad, /* NULL if not needed */
+                             void *f_data)
+{
+  // m-dim vector-valued constraints, n-dim joints
   my_constraint_data *d = (my_constraint_data *) data;
 
-  double a = d->a, b = d->b;
-    if (grad) {
-      grad[0] = 3 * a * (a*x[0] + b) * (a*x[0] + b);
-      grad[1] = -1.0;
+
+  // Compute gradients of constraint functions(if non-NULL, it points to an array with the size of m*n; access through)
+  if (grad){
+
+    for (unsigned i = 0; i < m; ++i)
+    {
+      for(unsigned j = 0; j < n; ++j)
+      {
+
+        grad[i * n + j] = 
+
+      }
+
     }
-  return ((a*x[0] + b) * (a*x[0] + b) * (a*x[0] + b) - x[1]);
+
+  }
+
+
+  // Compute constraints and store in `result`
+  result[i] = 
 
 
 }
@@ -54,55 +89,93 @@ int main(int argc, char **argv[])
   
 
   // Settings
-  const int joint_value_dim = 6; // the sequence of time differences is also the optimization variable
-  int num_datapoints = 1000;
-  my_constraint_data data; // set up constraint parameters
-  data.vel_limits = 
-  data.acc_limits = 
+  const unsigned int joint_value_dim = 6; 
+  unsigned int num_datapoints = 1000;
+
+  my_constraint_data constraint_data;
+  constraint_data.qlb = {};
+  constraint_data.qub = {};
 
 
-  // Set up optimiaztion
-  double lb[num_datapoints * joint_value_dim + num_datapoints - 1]; // default initialization to zeros
-  double ub[num_datapoints * joint_value_dim + num_datapoints - 1];
-  for (int i = 0; i < num_datapoints; ++i) // set joint limits
-  {
-    for (int j = 0; j < joint_value_dim; ++j)
-    {
-      lb[i * joint_value_dim + j] = data.jlb[j];
-      ub[i * joint_value_dim + j] = data.jub[j];
-    }
-  }
-  for (int t = 0; t < num_datapoints - 1; ++t) // set time difference's limit
-  {
-    lb[num_datapoints * joint_value_dim + t] = 0;
-    ub[num_datapoints * joint_value_dim + t] = HUGE_VAL;
-  }
+  std::vector<double> x(joint_value_dim); // optimization variables, default initialized to zeros
+  double minf; // for storing the minimum objective value
 
+  unsigned int num_constraints = joint_value_dim * 2; // upper+lower bounds for each JOINT
+  std::vector<double> vtol(num_constraints, 1e-8); // vector-valued tolerance
   
-  // Set up optimizer
-  nlopt::opt opt(nlopt::LD_SLSQP, num_datapoints * joint_value_dim + num_datapoints - 1);
-  nlopt_set_lower_bounds(opt, lb); // set lower bounds
-  nlopt_set_upper_bounds(opt, ub); // set upper bounds
-  nlopt::opt:set_min_objective(myfunc, NULL); // set objective function to minimize
-  nlopt_add_inequality_constraint(opt, myconstraint, &data[0], 1e-8); // inequality constraint for each optimization variable
-  nlopt_add_inequality_constraint(opt, myconstraint, &data[0], 1e-8); 
+  double tol = 1e-4;
+  double stopval = 1e-8;
 
+  // Set up optimizer
+  try
+    nlopt::opt opt(nlopt::LD_SLSQP, num_datapoints * joint_value_dim + num_datapoints - 1);
+  catch(std::bad_alloc e)
+  {
+    std::cout << "Something went wrong in the constructor: " << e << std::endl;
+  }
+  opt.set_min_objective(myfunc, NULL); // set objective function to minimize; with no additional information passed(f_data)
+  opt.set_lower_bounds(constraint_data.qlb); // set lower bounds
+  opt.set_upper_bounds(constraint_data.qub); // set upper bounds
+  opt.add_inequality_mconstraint(myconstraint, NULL, vtol); // vector-valued constraints(more convenient)
+  opt.set_stopval(stopval); // stop value
+  opt.set_ftol_rel(tol); // objective function value changes by less than `tol` multiplied by the absolute value of the function value
+  opt.set_ftol_abs(1e-8); // objective function value changes by less than `tol`
+  opt.set_xtol_rel(tol); // optimization parameters' magnitude changes by less than `tol` multiplied by the current magnitude(can set weights for each dimension)
+  opt.set_xtol_abs(1e-12); // optimization parameters' magnitude changes by less than `tol`
+  opt.set_maxeval(100); // maximum evaluation
+  opt.set_maxtime(1.0); // maximum time
+  
 
   // Start optimization
-  double x[num_datapoints * joint_value_dim + num_datapoints - 1]; // default initialization to zeros
-  double minf; // for storing the minimum objective value
-  if (nlopt::opt::optimize(opt, x, &minf) < 0)
+  try
   {
-    printf("NLopt failed!\n");
+    nlopt::result opt_result = opt.optimize(x, minf);
   }
-  else
+  catch (std::runtime_error e1)
   {
-    printf("NLopt found minimum f: %0.10g, after %d evaluations\n", minf, count);
+    std::cout << "Runtime error: " << e1 << std::endl;
   }
-  
+  catch (std::invalid_argument e2)
+  {
+    std::cout << "Invalid argument: " << e2 << std::endl;    
+  }
+  catch (std::bad_alloc e3)
+  {
+    std::cout << "Ran out of memory: " << e3 << std::endl;    
+  }
+  catch (nlopt::roundoff_limited e4)
+  {
+    std::cout << "Roundoff errors limited progress: " << e4 << std::endl;    
+  }
+  catch (nlopt::forced_stop e5)
+  {
+    std::cout << "Forced termination: " << e5 << std::endl;    
+  }
 
-  // Finish optimization
-  nlopt_destroy(opt);
+
+  // Display, store results and exit
+  switch(opt_result)
+  {
+    case nlopt::SUCCESS:
+      std::cout << "Optimization finished successfully." << std::endl;
+      break;
+    case nlopt::STOPVAL_REACHED:
+      std::cout << "Optimization terminated due to STOPVAL reached." << std::endl;
+      break;
+    case nlopt::FTOL_REACHED:
+      std::cout << "Optimization terminated due to FTOL reached." << std::endl;
+      break;
+    case nlopt::XTOL_REACHED:
+      std::cout << "Optimization terminated due to XTOL reached." << std::endl;
+      break;
+    case nlopt::MAXEVAL_REACHED:
+      std::cout << "Optimization terminated due to MAXEVAL reached." << std::endl;
+      break;
+    case nlopt::MAXTIME_REACHED`:
+      std::cout << "Optimization terminated due to MAXTIME reached." << std::endl;
+      break;
+  }
+  std::cout << "NLopt found minimum f: " << minf << " after " << opt.get_numevals() << " evaluations." << std::endl;
   
   return 0;
 
