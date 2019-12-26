4d060c0e10acd574d2117cd9f0af200271ba4177
liangyuwei <liangyuwei@yahoo.com>
2019-12-26 23:51:59 +0800

    Modify the code for loading visualization markers.(ongoing)

--- arm_hand_capture/src/load_visualization_markers.cpp
+++ arm_hand_capture/src/load_visualization_markers.cpp
@@ -16,6 +16,27 @@
 #include <Eigen/Dense>
 #include <Eigen/Geometry>
 
+/* Set a marker connecting the two points given with a sphere */
+// Note that nothing else but the pos is changed!!!
+void set_segment(Eigen::Vector3d point_1, Eigen::Vector3d point_2, visualization_msgs::Marker& marker)
+{
+  // Set positions
+  marker.pose.position.x = (point_1[0] + point_2[0]) / 2.0;
+  marker.pose.position.y = (point_1[1] + point_2[1]) / 2.0;
+  marker.pose.position.z = (point_1[2] + point_2[2]) / 2.0;
+
+  // Set orientation
+  Eigen::Vector3d base_z = {0.0, 0.0, 1.0};
+  Eigen::Quaterniond quat_shoulders = Eigen::Quaterniond::FromTwoVectors(base_z, point_1 - point_2);
+  marker.pose.orientation.x = quat_shoulders.x();
+  marker.pose.orientation.y = quat_shoulders.y();
+  marker.pose.orientation.z = quat_shoulders.z();
+  marker.pose.orientation.w = quat_shoulders.w();
+  marker.scale.x = 0.1;
+  marker.scale.y = 0.1;
+  marker.scale.z = (point_1 - point_2).norm();
+}
+
 
 class GetPosAndVisualize
 {
@@ -38,42 +59,55 @@ class GetPosAndVisualize
 void GetPosAndVisualize::posCallback(const arm_hand_capture::DualArmDualHandStateConstPtr& msg)
 {
 
-  // Prep
+  // Prepare Marker message
   visualization_msgs::MarkerArray marker_array;
-  //std::vector<visualization_msgs::Marker> marker_array;
   visualization_msgs::Marker marker;
   marker.header.frame_id = "world";
   marker.header.stamp = ros::Time();
   marker.ns = "sign_language_robot_ns"; // namespace?
-
-
-  // Draw shoulders
-  marker.id = 0;
   marker.type = visualization_msgs::Marker::SPHERE;
   marker.action = visualization_msgs::Marker::ADD;
-  Eigen::Vector3d r_up_pos = {msg->right_upperarm_pose.pose.position.x, msg->right_upperarm_pose.pose.position.y, msg->right_upperarm_pose.pose.position.z};
-  Eigen::Vector3d l_up_pos = {msg->left_upperarm_pose.pose.position.x, msg->left_upperarm_pose.pose.position.y, msg->left_upperarm_pose.pose.position.z};
-  Eigen::Vector3d base_z = {0.0, 0.0, 1.0};
-  Eigen::Quaterniond quat_shoulders = Eigen::Quaterniond::FromTwoVectors(base_z, l_up_pos - r_up_pos);
-  marker.pose.orientation.x = quat_shoulders.x();
-  marker.pose.orientation.y = quat_shoulders.y();
-  marker.pose.orientation.z = quat_shoulders.z();
-  marker.pose.orientation.w = quat_shoulders.w();
-  marker.scale.x = 0.1;
-  marker.scale.y = 0.1;
-  marker.scale.z = (r_up_pos - l_up_pos).norm();
   marker.color.a = 1.0; // visable
   marker.color.r = 0.0;
   marker.color.g = 1.0;
   marker.color.b = 0.0;
+
+
+  // Get temporary parameters
+  Eigen::Vector3d r_up_pos = {msg->right_upperarm_pose.pose.position.x, msg->right_upperarm_pose.pose.position.y, msg->right_upperarm_pose.pose.position.z};
+  Eigen::Vector3d r_fr_pos = {msg->right_forearm_pose.pose.position.x, msg->right_forearm_pose.pose.position.y, msg->right_forearm_pose.pose.position.z};
+  Eigen::Vector3d r_hd_pos = {msg->right_hand_pose.pose.position.x, msg->right_hand_pose.pose.position.y, msg->right_hand_pose.pose.position.z};
+
+  Eigen::Vector3d l_up_pos = {msg->left_upperarm_pose.pose.position.x, msg->left_upperarm_pose.pose.position.y, msg->left_upperarm_pose.pose.position.z};
+  Eigen::Vector3d l_fr_pos = {msg->left_forearm_pose.pose.position.x, msg->left_forearm_pose.pose.position.y, msg->left_forearm_pose.pose.position.z};
+  Eigen::Vector3d l_hd_pos = {msg->left_hand_pose.pose.position.x, msg->left_hand_pose.pose.position.y, msg->left_hand_pose.pose.position.z};
+
+
+  // Draw shoulders
+  marker.id = 0;
+  set_segment(l_up_pos, r_up_pos, marker);
   marker_array.markers.push_back(marker);
 
+
+  // Draw neck
+
+
   // Draw upperarm links
-  //marker.id = 0;
+  marker.id = 1; // left upperarm link
+  set_segment(l_fr_pos, l_up_pos, marker);
+  marker_array.markers.push_back(marker);
+  marker.id = 2; // right upperarm link
+  set_segment(r_fr_pos, r_up_pos, marker);
+  marker_array.markers.push_back(marker);
 
 
   // Draw forearm links
-  //marker.id = 0;
+  marker.id = 3; // left forearm link
+  set_segment(l_hd_pos, l_fr_pos, marker);
+  marker_array.markers.push_back(marker);
+  marker.id = 4; // right forearm link
+  set_segment(r_hd_pos, r_fr_pos, marker);
+  marker_array.markers.push_back(marker);
 
 
   // Draw hands
@@ -96,7 +130,7 @@ GetPosAndVisualize::GetPosAndVisualize()
 
   // Initialize a publisher
   ROS_INFO_STREAM("Bring up a publisher /visualization_marker_array...");  
-  vis_pub_ = n_.advertise<visualization_msgs::MarkerArray>("/visualization_marker_array", 0);
+  vis_pub_ = n_.advertise<visualization_msgs::MarkerArray>("/visualization_marker_array", 10);
   ROS_INFO_STREAM("Ready to publish visualization markers.");
 
 
