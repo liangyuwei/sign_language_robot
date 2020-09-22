0a4e3d9abfe90a620c26f5e9b0280fe8fd24909b
liangyuwei <liangyuwei@yahoo.com>
2020-09-22 22:53:28 +0800

    Implement visualization code for human IK results and for YuMi execting manually mapped human joint trajectories.

--- yumi_sign_language_robot_control/script/yumi_sign_robot_execute_arm_joint_path.py
+++ yumi_sign_language_robot_control/script/yumi_sign_robot_execute_arm_joint_path.py
@@ -18,34 +18,10 @@ import getopt # process the terminal arguments
 
 import h5py
 
-
-def all_close(goal, actual, tolerance):
-  """
-  Convenience method for testing if a list of values are within a tolerance of their counterparts in another list
-  @param: goal       A list of floats, a Pose or a PoseStamped
-  @param: actual     A list of floats, a Pose or a PoseStamped
-  @param: tolerance  A float
-  @returns: bool
-  """
-  all_equal = True
-  if type(goal) is list:
-    for index in range(len(goal)):
-      if abs(actual[index] - goal[index]) > tolerance:
-        return False
-
-  elif type(goal) is geometry_msgs.msg.PoseStamped:
-    return all_close(goal.pose, actual.pose, tolerance)
-
-  elif type(goal) is geometry_msgs.msg.Pose:
-    return all_close(pose_to_list(goal), pose_to_list(actual), tolerance)
-
-  return True
-
-
 def main():
 
-  file_name = "test_imi_data.h5"
-  group_name = "fengren_1"
+  file_name = "mocap_ik_results_YuMi.h5"
+  group_name = "baozhu_1"
 
   try:
     options, args = getopt.getopt(sys.argv[1:], "hf:g:", ["help", "file-name=", "group-name="])
@@ -67,24 +43,19 @@ def main():
       print("Name of the motion(group) inside the h5 file: {0}\n".format(value))
       group_name = value
 
-  print(">>>> Input file name: " + file_name)
-  print(">>>> Group name: " + group_name)
 
   try:
 
     ### Set up groups
     print("============ Set up group ...")
-    left_arm_group = moveit_commander.MoveGroupCommander("left_arm")
-    right_arm_group = moveit_commander.MoveGroupCommander("right_arm")
     dual_arms_group = moveit_commander.MoveGroupCommander("dual_arms")
 
 
     ### Read h5 file for joint paths (arms only for now)
     f = h5py.File(file_name, "r")
-    l_joint_angles_ik = f[group_name]['l_joint_angles_optim_ik'][:]
-    r_joint_angles_ik = f[group_name]['r_joint_angles_optim_ik'][:] # N x 7, 7 DOF and N path points
-    lr_joint_angles_ik = np.concatenate((l_joint_angles_ik, r_joint_angles_ik), axis=1)
-    timestamp = f[group_name]['time'][:] # N x 1
+    l_joint_angles_ik_yumi = f[group_name]['l_joint_angles_optim_ik_yumi']
+    r_joint_angles_ik_yumi = f[group_name]['r_joint_angles_optim_ik_yumi']
+    lr_joint_angles_ik_yumi = np.concatenate((l_joint_angles_ik_yumi, r_joint_angles_ik_yumi), axis=1)
     f.close()
 
 
@@ -92,9 +63,8 @@ def main():
     import pdb
     pdb.set_trace()
     print "============ Both arms go to initial positions..."
-    # group joints structure: left arm, right arm
     dual_arms_group.allow_replanning(True)
-    dual_arms_group.go(lr_joint_angles_ik[0, :], wait=True)
+    dual_arms_group.go(lr_joint_angles_ik_yumi[0, :], wait=True)
     dual_arms_group.stop()
 
     import pdb
@@ -105,15 +75,12 @@ def main():
     print "============ Construct a plan of two arms' motion..."
     cartesian_plan = moveit_msgs.msg.RobotTrajectory()
     cartesian_plan.joint_trajectory.header.frame_id = '/world'
-    cartesian_plan.joint_trajectory.joint_names = ['left_shoulder_1_joint', 'left_shoulder_2_joint', 'left_shoulder_3_joint', \
-                                                   'left_elbow_1_joint', 'left_elbow_2_joint', \
-                                                   'left_wrist_1_joint', 'left_wrist_2_joint', \
-                                                   'right_shoulder_1_joint', 'right_shoulder_2_joint', 'right_shoulder_3_joint', \
-                                                   'right_elbow_1_joint', 'right_elbow_2_joint', \
-                                                   'right_wrist_1_joint', 'right_wrist_2_joint']
-    for i in range(lr_joint_angles_ik.shape[0]):
+    cartesian_plan.joint_trajectory.joint_names = ['yumi_joint_1_l', 'yumi_joint_2_l', 'yumi_joint_7_l', 'yumi_joint_3_l', 'yumi_joint_4_l', 'yumi_joint_5_l', 'yumi_joint_6_l'] \
+    + ['yumi_joint_1_r', 'yumi_joint_2_r', 'yumi_joint_7_r', 'yumi_joint_3_r', 'yumi_joint_4_r', 'yumi_joint_5_r', 'yumi_joint_6_r'] 
+
+    for i in range(lr_joint_angles_ik_yumi.shape[0]):
         path_point = trajectory_msgs.msg.JointTrajectoryPoint()
-        path_point.positions = lr_joint_angles_ik[i].tolist()
+        path_point.positions = lr_joint_angles_ik_yumi[i].tolist()
         t = rospy.Time(i*1.0/5.0) 
         path_point.time_from_start.secs = t.secs
         path_point.time_from_start.nsecs = t.nsecs        
@@ -121,8 +88,6 @@ def main():
 
 
     ### Execute the plan
-    print "============ Execute the plan..."
-    # execute the plan
     print "============ Execute the planned path..."        
     dual_arms_group.execute(cartesian_plan, wait=True)
 
@@ -137,6 +102,7 @@ def main():
 if __name__ == '__main__':
 
   main()
+
 
 
 
