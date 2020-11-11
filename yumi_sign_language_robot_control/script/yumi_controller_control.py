54bb7885cb091fd0b0c6310d4d69db92c962b6cd
liangyuwei <liangyuwei@yahoo.com>
2020-11-11 14:08:52 +0800

    Switch back to EffortJointInterface and fine-tune PID parameters.
      - Add script for controlling arms and hands.

--- yumi_sign_language_robot_control/script/yumi_controller_control.py
+++ yumi_sign_language_robot_control/script/yumi_controller_control.py
@@ -24,138 +24,183 @@ from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 
 from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryActionGoal
 
-# Linear mapping: (x - x_min) / (x_max - x_min) == (x_hat - x_min_hat) / (x_max_hat - x_min_hat)
-def linear_map(x_min, x_max, x, x_min_hat, x_max_hat):
-  return (x - x_min) / (x_max - x_min) * (x_max_hat - x_min_hat) + x_min_hat
 
 
-### Process one path point 
-def sr_linear_map(human_joint_angles):
-  
-  ## initialization
-  sr_hand_dof = 22
-  srhand_joint_angles = np.zeros(sr_hand_dof)
+class YumiControl():
 
-  ## preparation (from start to final: flexion and abduction!!! note the direction of motion!!!)
-  abduction = 15 * math.pi / 180.0 # 0.35 max, approximately +-20 deg
-  # SR hand - # FF, MF, RF, LF, TH
-  # actually, one-to-one is not suitable for Shadowhand's thumb joints;
-  # structural difference from the dataglove model... measured data of dataglove cannot be simply one-to-one mapped 
-  sr_start = np.array([abduction, 0, 0, 0, \
-                       abduction/2, 0, 0, 0, \
-                       0, 0, 0, 0, \
-                       0, 0, 0, 0, 0, \
-                       0, 0, 0, 0, 0])
-  sr_final = np.array([-abduction, 1.56, 1.56, 1.56, \
-                       -abduction/2, 1.56, 1.56, 1.56, \
-                       -abduction, 1.56, 1.56, 1.56, \
-                        0, -abduction, 1.56, 1.56, 1.56, \
-                        0, 1.21, 0, 0.69, 1.56]) 
-  hm_start = np.array([0,    0, 53,  0,   0, 30,  0,   0, 22,  0,   0, 35,  0,   0,  0]) # modify index-middle abduction angle range to allow finger crossing..
-            # np.array([0,    0, 53,  0,   0, 22,  0,   0, 22,  0,   0, 35,  0,   0,  0])
-  hm_final = np.array([45, 100,  0, 90, 120,  0, 90, 120,  0, 90, 120,  0, 90, 120, 58]) # in dataglove 's sequence
-
-  ## one-to-one joint matching: 2 joint(wrist) + 22 joint(hand) = 24 joint; 2 DOFs(wrist) + 18 DOFs(hand) = 20 DOFs
-  # forefinger (abd/add -> flex/ext)
-  srhand_joint_angles[0] = linear_map(hm_start[5], hm_final[5], human_joint_angles[5], sr_start[0], sr_final[0])
-  srhand_joint_angles[1] = linear_map(hm_start[3], hm_final[3], human_joint_angles[3], sr_start[1], sr_final[1])
-  srhand_joint_angles[2] = linear_map(hm_start[4], hm_final[4], human_joint_angles[4], sr_start[2], sr_final[2])
-  srhand_joint_angles[3] = srhand_joint_angles[2] * 2.0 / 3.0 # two-thirds rule
-  # middle finger
-  srhand_joint_angles[4] = linear_map(hm_start[5], hm_final[5], human_joint_angles[5], 0.0, 0.0) # stick to neutral position
-  srhand_joint_angles[5] = linear_map(hm_start[6], hm_final[6], human_joint_angles[6], sr_start[5], sr_final[5])
-  srhand_joint_angles[6] = linear_map(hm_start[7], hm_final[7], human_joint_angles[7], sr_start[6], sr_final[6])
-  srhand_joint_angles[7] = srhand_joint_angles[6] * 2.0 / 3.0 # two-thirds rule
-  # ring finger
-  srhand_joint_angles[8] = linear_map(hm_start[8], hm_final[8], human_joint_angles[8], sr_start[8], sr_final[8])
-  srhand_joint_angles[9] = linear_map(hm_start[9], hm_final[9], human_joint_angles[9], sr_start[9], sr_final[9])
-  srhand_joint_angles[10] = linear_map(hm_start[10], hm_final[10], human_joint_angles[10], sr_start[10], sr_final[10])
-  srhand_joint_angles[11] = srhand_joint_angles[10] * 2.0 / 3.0 # two-thirds rule
-  # little finger
-  srhand_joint_angles[12] = 0.0 # joint between little finger and palm
-  srhand_joint_angles[13] = linear_map(hm_start[11], hm_final[11], human_joint_angles[11], sr_start[13], sr_final[13])
-  srhand_joint_angles[14] = linear_map(hm_start[12], hm_final[12], human_joint_angles[12], sr_start[14], sr_final[14])
-  srhand_joint_angles[15] = linear_map(hm_start[13], hm_final[13], human_joint_angles[13], sr_start[15], sr_final[15])
-  srhand_joint_angles[16] = srhand_joint_angles[15] * 2.0 / 3.0 # two-thirds rule
-  # forefinger
-  srhand_joint_angles[17] = 0.0 # fixed at 0, but there should be coupling...
-  srhand_joint_angles[18] = linear_map(hm_start[14], hm_final[14], human_joint_angles[14], sr_start[18], sr_final[18])
-  srhand_joint_angles[19] = 0.0 # fixed at 0, but there should be coupling...
-  srhand_joint_angles[20] = linear_map(hm_start[1], hm_final[1], human_joint_angles[1], sr_start[20], sr_final[20])
-  srhand_joint_angles[21] = linear_map(hm_start[0], hm_final[0], human_joint_angles[0], sr_start[21], sr_final[21])
-
-  return srhand_joint_angles
+  def __init__(self):
+    
+    ### 1 - Command controller via topic
+    
+    # set up a publisher
+    # self.arm_hand_pub = rospy.Publisher("/yumi/dual_arm_hand_joint_controller/command", JointTrajectory, queue_size=10)
+    self.arm_pub = rospy.Publisher("/yumi/dual_arm_joint_controller/command", JointTrajectory, queue_size=10)
+    self.hand_pub = rospy.Publisher("/yumi/dual_hand_joint_controller/command", JointTrajectory, queue_size=10)
 
 
-### Arrange mapping for a whole joint path
-def map_glove_to_srhand(human_hand_path):
-  # input is of the size (N x 30), containing data of two hands
+    ### 2 - Command controller via action server
+    self.arm_client = actionlib.SimpleActionClient("/yumi/dual_arm_joint_controller/follow_joint_trajectory", FollowJointTrajectoryAction)
+    self.arm_client.wait_for_server()
+    self.hand_client = actionlib.SimpleActionClient("/yumi/dual_hand_joint_controller/follow_joint_trajectory", FollowJointTrajectoryAction)
+    self.hand_client.wait_for_server()
 
-  # prep
-  sr_hand_dof = 22
-  sr_hand_path = np.zeros((human_hand_path.shape[0], 2*sr_hand_dof))
+    ### Prep
+    # self.joint_names = ['yumi_joint_1_l', 'yumi_joint_2_l', 'yumi_joint_7_l', 'yumi_joint_3_l', 'yumi_joint_4_l', 'yumi_joint_5_l', 'yumi_joint_6_l'] \
+    # + ['yumi_joint_1_r', 'yumi_joint_2_r', 'yumi_joint_7_r', 'yumi_joint_3_r', 'yumi_joint_4_r', 'yumi_joint_5_r', 'yumi_joint_6_r'] \
+    # + ['link1', 'link11', 'link2', 'link22', 'link3', 'link33', 'link4', 'link44', 'link5', 'link51', 'link52', 'link53'] \
+    # + ['Link1', 'Link11', 'Link2', 'Link22', 'Link3', 'Link33', 'Link4', 'Link44', 'Link5', 'Link51', 'Link52', 'Link53']
+    self.arm_joint_names = ['yumi_joint_1_l', 'yumi_joint_2_l', 'yumi_joint_7_l', 'yumi_joint_3_l', 'yumi_joint_4_l', 'yumi_joint_5_l', 'yumi_joint_6_l'] \
+    + ['yumi_joint_1_r', 'yumi_joint_2_r', 'yumi_joint_7_r', 'yumi_joint_3_r', 'yumi_joint_4_r', 'yumi_joint_5_r', 'yumi_joint_6_r'] 
+    self.hand_joint_names = ['link1', 'link11', 'link2', 'link22', 'link3', 'link33', 'link4', 'link44', 'link5', 'link51', 'link52', 'link53'] \
+    + ['Link1', 'Link11', 'Link2', 'Link22', 'Link3', 'Link33', 'Link4', 'Link44', 'Link5', 'Link51', 'Link52', 'Link53']
 
-  import pdb
-  pdb.set_trace()
-  print(">>>> Iterate to process the input dataglove data...")
-  # iterate to process
-  for n in range(human_hand_path.shape[0]):
-    print("== processing point {}/{}".format((n+1), human_hand_path.shape[0]))
-    # left 
-    sr_hand_path[n, :sr_hand_dof] = sr_linear_map(human_hand_path[n, :15])
-    # right 
-    sr_hand_path[n, sr_hand_dof:] = sr_linear_map(human_hand_path[n, 15:])
+  '''
+  def topic_control(self, q_goal):
+    # set up a JointTrajectory msg
+    arm_hand_cmd = self.set_arm_hand_joint_goal(q_goal)
 
-  return sr_hand_path
+    # publish
+    # rate = rospy.Rate(10)
+    # while not rospy.is_shutdown():
+    self.arm_hand_pub.publish(arm_hand_cmd)
+    # arm_hand_pub.publish(arm_hand_cmd)
+      # rate.sleep()
+  '''
 
 
-def set_arm_hand_joint_goal(q_goal):
-  # init
-  arm_hand_cmd = JointTrajectory()
-  waypoint = JointTrajectoryPoint()
-  # joint names
-  arm_hand_cmd.joint_names = ['yumi_joint_1_l', 'yumi_joint_2_l', 'yumi_joint_7_l', 'yumi_joint_3_l', 'yumi_joint_4_l', 'yumi_joint_5_l', 'yumi_joint_6_l'] \
-  + ['yumi_joint_1_r', 'yumi_joint_2_r', 'yumi_joint_7_r', 'yumi_joint_3_r', 'yumi_joint_4_r', 'yumi_joint_5_r', 'yumi_joint_6_r'] \
-  + ['link1', 'link11', 'link2', 'link22', 'link3', 'link33', 'link4', 'link44', 'link5', 'link51', 'link52', 'link53'] \
-  + ['Link1', 'Link11', 'Link2', 'Link22', 'Link3', 'Link33', 'Link4', 'Link44', 'Link5', 'Link51', 'Link52', 'Link53']
-  # append waypoints
-  waypoint.positions = q_goal
-  waypoint.time_from_start.secs = 1.0
-  arm_hand_cmd.points.append(waypoint)
-  # return
-  return arm_hand_cmd
+  def arm_topic_control(self, q_goal):
+    # set up a JointTrajectory msg
+    arm_cmd = self.set_arm_joint_goal(q_goal)
+
+    # publish
+    # rate = rospy.Rate(10)
+    # while not rospy.is_shutdown():
+    self.arm_pub.publish(arm_cmd)
+    # arm_hand_pub.publish(arm_hand_cmd)
+      # rate.sleep()
+
+
+  def hand_topic_control(self, q_goal):
+    # set up a JointTrajectory msg
+    hand_cmd = self.set_hand_joint_goal(q_goal)
+
+    # publish
+    # rate = rospy.Rate(10)
+    # while not rospy.is_shutdown():
+    self.hand_pub.publish(hand_cmd)
+    # arm_hand_pub.publish(arm_hand_cmd)
+      # rate.sleep()
+
+  '''
+  def action_control(self, q_goal):
+    print("== Waiting for action server... ")
+    self.client.wait_for_server()
+    # JointTrajectory
+    joint_goal = self.set_arm_hand_joint_goal(q_goal)
+    # FollowJointTrajectoryActionGoal
+    action_goal = FollowJointTrajectoryActionGoal()
+    action_goal.goal.trajectory = joint_goal
+    # send goal
+    self.client.send_goal(action_goal.goal)
+    self.client.wait_for_result()
+  '''
+
+  def arm_hand_action_control(self, q_arm_goal, q_hand_goal):
+    print("== Waiting for action server... ")
+    self.arm_client.wait_for_server()
+    self.hand_client.wait_for_server()
+
+    # JointTrajectory
+    arm_joint_goal = self.set_arm_joint_goal(q_arm_goal)
+    hand_joint_goal = self.set_arm_joint_goal(q_hand_goal)
+
+    # FollowJointTrajectoryActionGoal
+    arm_action_goal = FollowJointTrajectoryActionGoal()
+    arm_action_goal.goal.trajectory = arm_joint_goal
+    hand_action_goal = FollowJointTrajectoryActionGoal()
+    hand_action_goal.goal.trajectory = hand_joint_goal
+    
+    # send goals
+    self.arm_client.send_goal(arm_action_goal.goal)
+    self.hand_client.send_goal(hand_action_goal.goal)
+
+    # wait for results
+    self.arm_client.wait_for_result()
+    self.hand_client.wait_for_result()
+
+
+  def arm_action_control(self, q_goal):
+    print("== Waiting for action server... ")
+    self.arm_client.wait_for_server()
+    # JointTrajectory
+    joint_goal = self.set_arm_joint_goal(q_goal)
+    # FollowJointTrajectoryActionGoal
+    action_goal = FollowJointTrajectoryActionGoal()
+    action_goal.goal.trajectory = joint_goal
+    # send goal
+    self.arm_client.send_goal(action_goal.goal)
+    self.arm_client.wait_for_result()
+
+
+  def hand_action_control(self, q_goal):
+    print("== Waiting for action server... ")
+    self.hand_client.wait_for_server()
+    # JointTrajectory
+    joint_goal = self.set_hand_joint_goal(q_goal)
+    import pdb
+    pdb.set_trace()
+    # FollowJointTrajectoryActionGoal
+    action_goal = FollowJointTrajectoryActionGoal()
+    action_goal.goal.trajectory = joint_goal
+    # send goal
+    self.hand_client.send_goal(action_goal.goal)
+    self.hand_client.wait_for_result()
+
+
+  '''
+  def set_arm_hand_joint_goal(self, q_goal):
+    # init
+    arm_hand_cmd = JointTrajectory()
+    waypoint = JointTrajectoryPoint()
+    # joint names
+    arm_hand_cmd.joint_names = self.joint_names
+    # append waypoints
+    waypoint.positions = q_goal
+    waypoint.time_from_start.secs = 1.0
+    arm_hand_cmd.points.append(waypoint)
+    # return
+    return arm_hand_cmd
+  '''
+
+  def set_arm_joint_goal(self, q_goal):
+    # init
+    arm_cmd = JointTrajectory()
+    waypoint = JointTrajectoryPoint()
+    # joint names
+    arm_cmd.joint_names = self.arm_joint_names
+    # append waypoints
+    waypoint.positions = q_goal
+    waypoint.time_from_start.secs = 1.0
+    arm_cmd.points.append(waypoint)
+    # return
+    return arm_cmd
+
+
+  def set_hand_joint_goal(self, q_goal):
+    # init
+    hand_cmd = JointTrajectory()
+    waypoint = JointTrajectoryPoint()
+    # joint names
+    hand_cmd.joint_names = self.hand_joint_names
+    # append waypoints
+    waypoint.positions = q_goal
+    waypoint.time_from_start.secs = 1.0
+    hand_cmd.points.append(waypoint)
+    # return
+    return hand_cmd
 
 
 def main():
-
-  file_name = "glove-calib-2020-11-02.h5"
-  test_seq_name = 'test_finger_1/glove_angle' # 'test_finger_1_calibrated'
-  use_old_version = False
-
-  try:
-    options, args = getopt.getopt(sys.argv[1:], "hf:t:o", ["help", "file-name=", "test-seq-name=", "old-version"])
-  except getopt.GetoptError:
-    sys.exit()
-
-  for option, value in options:
-    if option in ("-h", "--help"):
-      print("Help:\n")
-      print("   This script executes the IK results.\n")
-      print("Arguments:\n")
-      print("   -f, --file-name=, specify the name of the h5 file to read joint trajectory from, suffix is required.\n")
-      print("   -t, --test-seq-name=, specify the name of the test sequence to visualize.\n")
-      print("   -o, --old-version, specify to use the data collected by old version of dataglove (S14+).\n")
-      exit(0)
-    if option in ("-f", "--file-name"):
-      print("Name of the h5 file to read joint trajectory from: {0}\n".format(value))
-      file_name = value
-    if option in ("-t", "--test-seq-name"):
-      print("Name of the test sequence data: {0}\n".format(value))
-      test_seq_name = value
-    if option in ("-o", "--old-version"):
-      print("Using the old version of dataglove data...")
-      use_old_version = True
-
 
   try:
 
@@ -164,22 +209,72 @@ def main():
 
 
     ### Prep
-    q_initial = [-1.5, -2.0, 1.5, 0, 0, 0, 0] \
-    + [1.5, -2.0, -1.5, 0, 0, 0, 0] \
-    + [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] \
+    # hand
+    q_hand_open = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] \
     + [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  
-    q_tpose = [-0.9, -1.3, 2.0, -1.6, 0, 0, 0] \
-    + [0.9, -1.3, -2.0, -1.6, 0, 0, 0] \
-    + [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] \
-    + [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]   # palm down
-    q_tpose2 = [-0.9, -1.3, 2.0, -1.6, 0, 0, -3.0] \
-    + [0.9, -1.3, -2.0, -1.6, 0, 0, 3.0] \
-    + [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] \
-    + [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]   # palm up
-    q_hug = [-1.42, -0.5, 1.57, -1.0, 0, 0, -0.7] \
-    + [1.42, -0.5, -1.57, -1.0, 0, 0, 0.7] \
-    + [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] \
-    + [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]   
+
+    q_hand_close = [-1.57, -1.57, -1.57, -1.57, -1.57, -1.57, -1.57, -1.57, 0, 0, 0, 0] \
+    + [-1.57, -1.57, -1.57, -1.57, -1.57, -1.57, -1.57, -1.57, 0, 0, 0, 0] 
+
+    q_hand_thumb_flat = [0, 0, 0, 0, 0, 0, 0, 0, 0.25, 0, 0, 0] \
+    + [0, 0, 0, 0, 0, 0, 0, 0, 0.25, 0, 0, 0] 
+
+    q_hand_thumb_rot = [0, 0, 0, 0, 0, 0, 0, 0, -0.95, 0, 0, 0] \
+    + [0, 0, 0, 0, 0, 0, 0, 0, -0.95, 0, 0, 0] 
+
+    # arm
+    q_arm_initial = [-1.5, -2.0, 1.5, 0, 0, 0, 0] \
+    + [1.5, -2.0, -1.5, 0, 0, 0, 0] 
+    
+    q_arm_tpose = [-0.9, -1.3, 2.0, -1.6, 0, 0, 0] \
+    + [0.9, -1.3, -2.0, -1.6, 0, 0, 0]  # palm down
+
+    q_arm_tpose2 = [-0.9, -1.3, 2.0, -1.6, 0, 0, -3.0] \
+    + [0.9, -1.3, -2.0, -1.6, 0, 0, 3.0] # palm up
+
+    q_arm_hug = [-1.42, -0.5, 1.57, -1.0, 0, 0, -0.7] \
+    + [1.42, -0.5, -1.57, -1.0, 0, 0, 0.7] 
+
+
+    ### Set up the control class
+    yumi_control = YumiControl()
+
+
+    ### Move to some pre-defined poses for debugging
+
+    # move to an initial state
+    yumi_control.arm_action_control(q_arm_hug)
+
+    import pdb
+    # arm-hand control
+    pdb.set_trace()
+    yumi_control.arm_hand_action_control(q_arm_initial, q_hand_open)
+
+    pdb.set_trace()
+    yumi_control.arm_hand_action_control(q_arm_hug, q_hand_close)
+
+    # arm control
+    pdb.set_trace()
+    yumi_control.arm_action_control(q_arm_initial)
+
+    pdb.set_trace()
+    yumi_control.arm_action_control(q_arm_tpose)
+
+    pdb.set_trace()
+    yumi_control.arm_action_control(q_arm_tpose2)
+
+    pdb.set_trace()
+    yumi_control.arm_action_control(q_arm_hug)
+
+    # hand control
+    pdb.set_trace()
+    yumi_control.hand_action_control(q_hand_open)
+
+    pdb.set_trace()
+    yumi_control.hand_action_control(q_hand_close)
+
+    pdb.set_trace()
+
 
     ### 1 - Command controller via topic
     '''
@@ -204,10 +299,11 @@ def main():
     '''
 
     ### 2 - Command controller via action server
+    '''
     client = actionlib.SimpleActionClient("/yumi/dual_arm_hand_joint_controller/follow_joint_trajectory", FollowJointTrajectoryAction)
     client.wait_for_server()
     # JointTrajectory
-    joint_goal = set_arm_hand_joint_goal(q_initial)
+    joint_goal = set_arm_hand_joint_goal(q_tpose)#(q_initial)
     # FollowJointTrajectoryActionGoal
     action_goal = FollowJointTrajectoryActionGoal()
     import pdb
@@ -216,9 +312,8 @@ def main():
     # send goal
     client.send_goal(action_goal.goal)
     client.wait_for_result()
+    '''
 
-    import pdb
-    pdb.set_trace()
 
   except rospy.ROSInterruptException:
     return
